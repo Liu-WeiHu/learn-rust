8a76bd4efdfdaa8925fe938f0bfa856d7e185af8
Liu-WeiHu <6460176@qq.com>
2021-07-05 09:31:04 +0000

    update

--- test/test-example/src/lib.rs
+++ test/test-example/src/lib.rs
@@ -424,3 +424,53 @@ fn test_deref2() {
     let hello = cow.into_owned();
 
 }
+
+mod tests {
+    use std::str::FromStr;
+    use std::io::{Error, ErrorKind};
+    use serde::Serialize;
+
+    #[derive(Debug, Serialize)]
+    struct Question {
+        id: QuestionId,
+        title: String,
+        content: String,
+        tags: Option<Vec<String>>,
+    }
+
+    #[derive(Debug, Serialize)]
+    struct QuestionId(String);
+
+    impl Question {
+        fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
+            Question {
+                id,
+                title,
+                content,
+                tags,
+            }
+        }
+    }
+
+    impl FromStr for QuestionId {
+        type Err = Error;
+
+        fn from_str(id: &str) -> Result<Self, Self::Err> {
+            match id.is_empty() {
+                false => Ok(QuestionId(id.to_string())),
+                true => Err(Error::new(ErrorKind::InvalidInput, "no id provided")),
+            }
+        }
+    }
+
+    #[test]
+    fn test_main() {
+        let question = Question::new(
+            QuestionId::from_str("1").expect("No id provided"),
+            "First Question".to_string(),
+            "Content of question".to_string(),
+            Some(vec!("faq".to_string())),
+        );
+        println!("{:?}", question);
+    }
+}
